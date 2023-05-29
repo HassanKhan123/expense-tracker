5cb1c29870d0e7dc682b3c176ce392cfacc47d63
HassanKhan123 <hk2602809@gmail.com>
2023-05-29 10:28:44 +0500

    added: close modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -11,10 +11,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -32,13 +34,27 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration:
+                const InputDecoration(label: Text('Amount'), prefixText: '\$ '),
+          ),
           Row(
             children: [
               ElevatedButton(
-                  onPressed: () {
-                    print(_titleController.text);
-                  },
-                  child: const Text('Save Expense'))
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              )
             ],
           )
         ],
