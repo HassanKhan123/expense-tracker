b2f1f1d7141a2b391fdf3f2c50932f6af2bd95cb
HassanKhan123 <hk2602809@gmail.com>
2023-05-30 15:11:32 +0500

    added: use theme inside of widgets

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -13,10 +13,20 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: expenses.length,
-        itemBuilder: (ctx, index) => Dismissible(
-            key: ValueKey(expenses[index]),
-            onDismissed: (direction) => onRemoveExpense(expenses[index]),
-            child: ExpensesItem(expenses[index])));
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onRemoveExpense(expenses[index]),
+        child: ExpensesItem(
+          expenses[index],
+        ),
+      ),
+    );
   }
 }
