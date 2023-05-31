efc655f58a77fc0c0919d6ea44f07f231fe0d104
HassanKhan123 <hk2602809@gmail.com>
2023-05-31 10:51:14 +0500

    added: locking the device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme =
@@ -8,7 +9,9 @@ var kDarkColorScheme = ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((fn) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
